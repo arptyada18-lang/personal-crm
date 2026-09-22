3aedb390ec871f375633b00ba72bd519e0918558
arptyada18-lang <arptyada18@gmail.com>
2026-09-22 09:36:26 +0530

    Add Personal CRM V7 offline service worker

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE="personal-crm-v7";
+const ASSETS=[
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./v2.js",
+  "./v3.js",
+  "./v4.js",
+  "./v5.js",
+  "./v6.js",
+  "./v7.js",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>{
+      const network=fetch(event.request).then(response=>{
+        if(response&&response.ok&&new URL(event.request.url).origin===self.location.origin){
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+        }
+        return response;
+      }).catch(()=>cached);
+      return cached||network;
+    })
+  );
+});
